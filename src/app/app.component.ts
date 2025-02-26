dc21df927c6e74d512a159a9651642c3d157ac98
System Administrator <root@SJH-706-38.local>
2025-02-26 12:27:27 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormComponent } from './form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormComponent,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSliderModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatTooltipModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css'
+})
+export class FormComponent {
+  trainerName: string = '';
+  email: string = '';
+  password: string = '';
+  gender: string = '';
+  birthDate!: Date;
+  address: string = '';
+  badgeNumber: number = 5;
+  submitted = false;
+  minSkillLevel = 1;
+  maxSkillLevel = 8;
+
+  formdata: FormGroup = new FormGroup({
+    trainerName: new FormControl(''),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    gender: new FormControl('', [Validators.required]),
+    birthDate: new FormControl(null, [Validators.required]),
+    address: new FormControl(''),
+    badgeNumber: new FormControl(5),
+    region: new FormControl('', [Validators.required]),
+    terms: new FormControl(false, [Validators.requiredTrue])
+  });
+
+  onClickSubmit(data: {
+    trainerName: string;
+    email: string;
+    password: string;
+    gender: string;
+    birthDate: Date;
+    address: string;
+    badgeNumber: number;
+    region: string;
+    terms: boolean;
+  }) {
+    this.submitted = true;
+    this.trainerName = data.trainerName;
+    this.email = data.email;
+    this.password = data.password;
+    this.gender = data.gender;
+    this.birthDate = data.birthDate;
+    this.address = data.address;
+    this.badgeNumber = data.badgeNumber;
+
+    if (this.formdata.valid) {
+      console.log(this.formdata.value);
+    } else {
+      console.log('Form is not valid');
+    }
+  }
+}
